369604548b5041828be4c9c02fff685d122d4c6c
Luis Lagardera <luislagardera@gmail.com>
2019-07-13 12:56:39 -0300

    First Commit

---
+++ src/controllers/image.js
@@ -0,0 +1,23 @@
+const ctrl = {};
+
+ctrl.index = (req, res) => {
+	res.send('Página Image');
+};
+
+ctrl.create = (req, res) => {
+	res.send('Página Image');
+};
+
+ctrl.like = (req, res) => {
+	res.send('Página Image');
+};
+
+ctrl.comment = (req, res) => {
+	res.send('Página Image');
+};
+
+ctrl.remove = (req, res) => {
+	res.send('Página Image');
+};
+
+module.exports = ctrl;
